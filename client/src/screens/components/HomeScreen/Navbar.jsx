10ba231f77e5c6b93f9c3f55cdcc9faacf806eed
Tran Truong Van <Kingchenobama711@gmail.com>
2022-10-18 15:12:33 +0700

    King Chen, display projects in navbar, 18/10/2022

--- client/src/screens/components/HomeScreen/Navbar.jsx
+++ client/src/screens/components/HomeScreen/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LogoSvg from "../../../images/logo.svg";
 import HomeLight from "../../../images/Home.svg";
 import HomeDark from "../../../images/HomeBlack.svg";
@@ -20,6 +20,8 @@ import MarketingProject from "../../../images/MarketingProject.svg";
 import SearchIcon from "../../../images/Search.svg";
 import AddIcon from "../../../images/Add.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { LOCAL_STORAGE_TOKEN_NAME, URL_BASE } from "../../../contexts/constants.js"
 
 function Navbar(props) {
   /** STATE ONCLICK NAVITEM **/
@@ -35,39 +37,54 @@ function Navbar(props) {
   const [isHideProject, setIsHideProject] = React.useState(false);
   const [valueSearchProject, setValueSearchProject] = React.useState("");
 
+  const [recentProjects, setRecentProjects] = React.useState(null);
+
+  const getRecentProjects = async () => {
+    const response = await axios
+      .get(`${URL_BASE}/api/project/recent`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(
+            LOCAL_STORAGE_TOKEN_NAME
+          )}`,
+        },
+      })
+      .catch((err) => console.log(err));
+    if (response.data.success) {
+      setRecentProjects(response.data.projects);
+    }
+  };
+
+  useEffect(() => {
+    getRecentProjects();
+  }, []);
+
+  const [favoriteProjects, setFavoriteProjects] = React.useState(null);
+  const getFavoriteProjects = async () => {
+    const response = await axios
+      .get(`${URL_BASE}/api/project/favorite`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(
+            LOCAL_STORAGE_TOKEN_NAME
+          )}`,
+        },
+      })
+      .catch((err) => console.log(err));
+    if (response.data.success) {
+      setFavoriteProjects(response.data.projects);
+    }
+  };
+
+  useEffect(() => {
+    getFavoriteProjects();
+  }, []);
+
   /** Favourites project component **/
   const FavouritesProject = (props) => {
-    var backgroundApp = "bg-[#FFE0EC]",
-      backgroundWeb = "bg-[#D4E6FF]",
-      backgroundCreative = "bg-[#FFDCC7]",
-      backgroundMarketing = "bg-[#DDE1FF]";
+    const { name, avatar } = props.project;
     return (
       <div className="flex flex-row items-center mt-[10px]">
-        <div
-          className={`${
-            props.type === "app"
-              ? backgroundApp
-              : props.type === "web"
-              ? backgroundWeb
-              : props.type === "marketing"
-              ? backgroundMarketing
-              : props.type === "creative" && backgroundCreative
-          } w-[24px] h-[24px] rounded-[15px] flex items-center justify-center mr-[10px]`}
-        >
-          <img
-            src={
-              props.type === "app"
-                ? AppProject
-                : props.type === "web"
-                ? WebProject
-                : props.type === "creative"
-                ? CreativeProject
-                : props.type === "marketing" && MarketingProject
-            }
-            alt="Project Icon"
-          />
-        </div>
-        <span className="text-[13px]">{props.name}</span>
+        <img className=" w-[24px] h-[24px] rounded-full mr-[10px]" src={avatar} alt="Project Icon" />
+        <span className="text-[13px]">{name}</span>
       </div>
     );
   };
@@ -84,9 +101,8 @@ function Navbar(props) {
         <div className="mt-[25px]">
           <Link
             to="/"
-            className={`flex flex-row items-center py-[5px] px-[10px] rounded-[7px] ${
-              isClickedHome ? "bg-cyan" : "bg-white"
-            }`}
+            className={`flex flex-row items-center py-[5px] px-[10px] rounded-[7px] ${isClickedHome ? "bg-cyan" : "bg-white"
+              }`}
             onClick={() => {
               setIsClickedHome(true);
               setIsClickedTasks(false);
@@ -102,18 +118,16 @@ function Navbar(props) {
               className="h-[16px] w-[16px]"
             />
             <span
-              className={`text-[13px] ${
-                isClickedHome ? "text-white" : "text-black-20"
-              } ml-[10px] mt-[1px]`}
+              className={`text-[13px] ${isClickedHome ? "text-white" : "text-black-20"
+                } ml-[10px] mt-[1px]`}
             >
               Home
             </span>
           </Link>
           <Link
             to="/tasks"
-            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${
-              isClickedTasks ? "bg-cyan" : "bg-white"
-            }`}
+            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${isClickedTasks ? "bg-cyan" : "bg-white"
+              }`}
             onClick={() => {
               setIsClickedHome(false);
               setIsClickedTasks(true);
@@ -129,17 +143,15 @@ function Navbar(props) {
               className="h-[16px] w-[16px]"
             />
             <span
-              className={`text-[13px] ${
-                isClickedTasks ? "text-white" : "text-black-20"
-              } ml-[10px] mt-[1px]`}
+              className={`text-[13px] ${isClickedTasks ? "text-white" : "text-black-20"
+                } ml-[10px] mt-[1px]`}
             >
               My Tasks
             </span>
           </Link>
           <div
-            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${
-              isClickedPlan ? "bg-cyan" : "bg-white"
-            }`}
+            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${isClickedPlan ? "bg-cyan" : "bg-white"
+              }`}
             onClick={() => {
               setIsClickedHome(false);
               setIsClickedTasks(false);
@@ -155,17 +167,15 @@ function Navbar(props) {
               className="h-[16px] w-[16px]"
             />
             <span
-              className={`text-[13px] ${
-                isClickedPlan ? "text-white" : "text-black-20"
-              } ml-[10px] mt-[1px]`}
+              className={`text-[13px] ${isClickedPlan ? "text-white" : "text-black-20"
+                } ml-[10px] mt-[1px]`}
             >
               My Plan
             </span>
           </div>
           <div
-            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${
-              isClickedInbox ? "bg-cyan" : "bg-white"
-            }`}
+            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${isClickedInbox ? "bg-cyan" : "bg-white"
+              }`}
             onClick={() => {
               setIsClickedHome(false);
               setIsClickedTasks(false);
@@ -181,17 +191,15 @@ function Navbar(props) {
               className="h-[16px] w-[16px]"
             />
             <span
-              className={`text-[13px] ${
-                isClickedInbox ? "text-white" : "text-black-20"
-              } ml-[10px] mt-[1px]`}
+              className={`text-[13px] ${isClickedInbox ? "text-white" : "text-black-20"
+                } ml-[10px] mt-[1px]`}
             >
               Inbox
             </span>
           </div>
           <div
-            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${
-              isClickedPeople ? "bg-cyan" : "bg-white"
-            }`}
+            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${isClickedPeople ? "bg-cyan" : "bg-white"
+              }`}
             onClick={() => {
               setIsClickedHome(false);
               setIsClickedTasks(false);
@@ -207,17 +215,15 @@ function Navbar(props) {
               className="h-[16px] w-[16px]"
             />
             <span
-              className={`text-[13px] ${
-                isClickedPeople ? "text-white" : "text-black-20"
-              } ml-[10px] mt-[1px]`}
+              className={`text-[13px] ${isClickedPeople ? "text-white" : "text-black-20"
+                } ml-[10px] mt-[1px]`}
             >
               People
             </span>
           </div>
           <div
-            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${
-              isClickedReport ? "bg-cyan" : "bg-white"
-            }`}
+            className={`flex flex-row items-center mt-[5px] py-[5px] px-[10px] rounded-[7px] ${isClickedReport ? "bg-cyan" : "bg-white"
+              }`}
             onClick={() => {
               setIsClickedHome(false);
               setIsClickedTasks(false);
@@ -233,9 +239,8 @@ function Navbar(props) {
               className="h-[16px] w-[16px]"
             />
             <span
-              className={`text-[13px] ${
-                isClickedReport ? "text-white" : "text-black-20"
-              } ml-[10px] mt-[1px]`}
+              className={`text-[13px] ${isClickedReport ? "text-white" : "text-black-20"
+                } ml-[10px] mt-[1px]`}
             >
               Reporting
             </span>
@@ -257,19 +262,20 @@ function Navbar(props) {
             <img
               src={ExpandArrow}
               alt=""
-              className={`ease-in duration-150 ${
-                isOpenFavourites ? "rotate-0" : "rotate-180"
-              }`}
+              className={`ease-in duration-150 ${isOpenFavourites ? "rotate-0" : "rotate-180"
+                }`}
             />
           </div>
           <div className="overflow-hidden">
             <div
-              className={`${
-                isHideFavourProject && "mt-[-100%]"
-              } transition-all`}
+              className={`${isHideFavourProject && "mt-[-100%]"
+                } transition-all`}
             >
-              <FavouritesProject type="app" name="App Development" />
-              <FavouritesProject type="web" name="Web Project" />
+              {
+                favoriteProjects?.map(project =>
+                  <FavouritesProject key={project._id} project={project} />
+                )
+              }
             </div>
           </div>
         </div>
@@ -302,10 +308,9 @@ function Navbar(props) {
           <span className="text-black-20 text-[15px] font-medium">
             Projects
           </span>
-          <FavouritesProject type="app" name="App Development" />
-          <FavouritesProject type="web" name="Web Design" />
-          <FavouritesProject type="creative" name="Creative Project" />
-          <FavouritesProject type="marketing" name="Marketing & Sales" />
+          {recentProjects?.map(project =>
+            <FavouritesProject key={project._id} project={project} />)
+          }
         </div>
       </div>
     </div>
